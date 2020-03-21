286bd6933a7b90545f60b7cc3900669db5507aad
Zorgulle <winy95battle@gmail.com>
2020-03-21 21:44:56 +0100

    add insert methid

--- fifo/fifo.py
+++ fifo/fifo.py
@@ -1,2 +1,10 @@
 class Fifo:
-    pass
+    def __init__(self):
+        self.__fifo = list()
+
+    def insert(self, to_insert: object) -> None:
+        self.__fifo.append(to_insert)
+
+
+    def __len__(self):
+        return len(self.__fifo)
--- tests/test_fifo.py
+++
@@ -1,9 +0,0 @@
-from fifo import __version__
-from fifo import Fifo
-
-def test_fifo_instance():
-    fifo = Fifo()
-    assert fifo
-
-def test_version():
-    assert __version__ == '0.1.0'
